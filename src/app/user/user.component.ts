f3816c1ddf612b80a98b48363ee073aae3bde328
Khoa Le <khoa6430@gmail.com>
2025-04-08 11:51:33 +0700

    [Part-2.21] Xuất nội dung dynamic ra UI

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,6 +3,7 @@ import { DUMMY_USERS } from '../dummy-users';
 
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+console.log(randomIndex)
 @Component({
   selector: 'app-user',
   imports: [],
