da624c28f5c8607dcca5b294c40c3e2465e83fc6
Khoa Le <khoa6430@gmail.com>
2025-04-08 15:12:03 +0700

    [Part-2.30] Tạo input props để nhận từ component cha

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -7,7 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
- 
+  @Input() avatar!: string;
+  @Input() name!: string;
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+  
   onSelectUser() {
     
   }
