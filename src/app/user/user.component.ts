f2a324f8f5568429462705320eed9ea528f12fa8
Khoa Le <khoa6430@gmail.com>
2025-04-08 11:45:55 +0700

    [Part-2.20] Lưu data user trong class component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,5 +10,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS[randomIndex]
 }
