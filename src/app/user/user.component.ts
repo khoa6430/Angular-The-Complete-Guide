00863c9b38561f54180e11fda5224d5c29bc1e8f
Khoa Le <khoa6430@gmail.com>
2025-04-08 15:13:51 +0700

    [Part-2.31] Thêm required và  optional cho Inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,13 +7,13 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
-  
+
   onSelectUser() {
     
   }
