50a276307dafa3ec39c42ce8ad8337c047f4dfc7
Khoa Le <khoa6430@gmail.com>
2025-04-09 11:33:28 +0700

    [Part-2.46] Thêm các props để task compoent con show ra UI

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+interface Task {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
 @Component({
   selector: 'app-task',
   imports: [],
@@ -7,5 +13,5 @@ import { Component } from '@angular/core';
   styleUrl: './task.component.scss'
 })
 export class TaskComponent {
-
+  @Input({required: true}) task!: Task;
 }
