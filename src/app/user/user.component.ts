54510a038083fdf0e4b14d89133c56ed59264726
Khoa Le <khoa6430@gmail.com>
2025-04-09 11:40:21 +0700

    [Part-2.47] Tạo file model riêng để tách interface

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-interface User {
-  id: string;
-  avatar: string;
-  name: string;
-}
+import { User } from './user.model';
+
 @Component({
   selector: 'app-user',
   imports: [],
