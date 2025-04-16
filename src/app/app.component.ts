ee482ca74d4a87fbcc7b0e1e81eded64a62df64b
Khoa Le <khoa6430@gmail.com>
2025-04-16 15:01:53 +0700

    [Part-8.163] Sử dụng pipe để format ngày , nhiệt độ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { DatePipe, DecimalPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [DatePipe, DecimalPipe]
 })
 export class AppComponent {
   currentDate = new Date();
-  currentTemperaturs = {
+  currentTemperatures = {
     berlin: 4.2749812,
     newYork: 18.1214,
     paris: 72.1209001,
