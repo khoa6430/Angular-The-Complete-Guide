b43a5d679edc862310dd7734838df41d8fa78772
Khoa Le <khoa6430@gmail.com>
2025-04-17 14:30:06 +0700

    [Part-8-.184] Inject service vào service khác

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,12 +1,15 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 
 import { Task, TaskStatus } from './task.model';
+import { LoggingService } from '../logging.service';
 
-@Injectable({
-  providedIn: 'root',
+@Injectable({ 
+  providedIn: 'root' // giúp Angular tự động cung cấp LoggingService từ root injector → không cần thêm providers ở component nào cả.
 })
+
 export class TasksService {
   private tasks = signal<Task[]>([]);
+  private loggingService = inject(LoggingService);
 
   allTasks = this.tasks.asReadonly();
 
@@ -17,6 +20,7 @@ export class TasksService {
       status: 'OPEN',
     };
     this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+    this.loggingService.log('ADDED TASK WITH TITLE ' + taskData.title);
   }
 
   updateTaskStatus(taskId: string, newStatus: TaskStatus) {
@@ -25,5 +29,6 @@ export class TasksService {
         task.id === taskId ? { ...task, status: newStatus } : task
       )
     );
+    this.loggingService.log('CHANGE TASK STATUS TO ' + newStatus);
   }
 }
